60ff2ea6b1b816e598782baab4782d797930ba10
adamwinebarger <adam.k.winebarger@gmail.com>
2023-07-12 02:46:22 -0400

    Adding this starter code here since the Udemy course didnt have it. In case we end up needing a fall back after messing up the Firebase setup

---
+++ shopping_list_app/lib/widget/grocery_list.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+//import 'package:shopping_list_app/data/dummy_items.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
+import 'new_item.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+
+}
+
+class _GroceryListState extends State<GroceryList> {
+
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+        MaterialPageRoute(builder: (context) => const NewItem())
+    );
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget content = const Center(child: Text("You have no items"),);
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) => Dismissible(
+            key: ValueKey(_groceryItems[index].id),
+            direction: DismissDirection.startToEnd,
+            onDismissed: (direction) {
+                _removeItem(_groceryItems[index]);
+            },
+            child: ListTile(
+              title: Text(_groceryItems[index].name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: _groceryItems[index].category.color,
+              ),
+              trailing: Text(_groceryItems[index].quantity.toString()),
+            ),
+        )
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text ("Your groceries"),
+        actions: [
+          IconButton(
+              onPressed: _addItem,
+              icon: const Icon(Icons.add)
+          )
+        ],
+      ),
+      body: content
+    );
+  }
+
+
+
+}
